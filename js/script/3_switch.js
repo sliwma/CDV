af164bfa97fbc5a9aa238170d6e2d2da50af3387
sliwma <sliwma@gmail.com>
2019-04-15 18:04:09 +0200

    switch

--- js/script/3_switch.js
+++ js/script/3_switch.js
@@ -2,20 +2,24 @@ let elkomunikat = document.getElementById('komunikat');
 let x = prompt('Podaj co chcesz oliczyć', 'k - kwadrat, p - prostokąt, t - trójkąt');
 let pole, boka, bokb;
 
-if (x == 'k') {
+switch (x) {
+  case 'k':
   boka = prompt('Podaj długość boku a');
   pole = Math.pow(boka, 2)
   elkomunikat.innerHTML = 'Pole kwadratu wynosi: ' + pole + 'cm<sup>2</sup>';
-} else if (x == 'p') {
+  break;
+  case 'p':
   boka = prompt('Podaj długość boku a');
   bokb = prompt('Podaj długość boku b');
   pole = boka*bokb
   elkomunikat.innerHTML = 'Pole prostokąta wynosi: ' +  pole + 'cm<sup>2</sup>';
-} else if (x == 't') {
+  break;
+  case 't':
   boka = prompt('Podaj długość podstawy');
   bokb = prompt('Podaj wysokość');
   pole = 0.5 * boka * bokb
   elkomunikat.innerHTML = 'Pole trójkąta wynosi: ' +  pole + 'cm<sup>2</sup>';
-} else {
-  elkomunikat.innerHTML = '<span style="color:red">Błędne dane!</span>'
+  break;
+  default: elkomunikat.innerHTML = '<span style="color:red">Błędne dane!</span>';
+
 }
